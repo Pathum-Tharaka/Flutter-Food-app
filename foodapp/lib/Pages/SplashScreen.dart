471649d6b9cf4e4b7356b610e6d219fb0a012215
pathumtharaka2000 <it22600516@my.sliit.lk>
2024-09-29 20:16:09 +0530

    food app

---
+++ foodapp/lib/Pages/SplashScreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: Image.asset("images/Splash.jpeg"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
