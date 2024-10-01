3334525f9fb8cfdf6f1a3a2f19fd6f37801bfd80
pathumtharaka2000 <it22600516@my.sliit.lk>
2024-10-02 01:17:03 +0530

    Update main.dart

--- foodapp/lib/main.dart
+++ foodapp/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       routes: {
-        "/": (context) => SplashScreen(),
+        "/": (context) => const SplashScreen(),
         "homePage": (context) => HomePage(),
       },
     );
