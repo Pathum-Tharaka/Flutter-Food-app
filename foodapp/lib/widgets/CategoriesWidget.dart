9c227d297c0da99f73e5bb1990d4e862de898d44
pathumtharaka2000 <it22600516@my.sliit.lk>
2024-10-02 12:16:55 +0530

    Update CategoriesWidget.dart

--- foodapp/lib/widgets/CategoriesWidget.dart
+++ foodapp/lib/widgets/CategoriesWidget.dart
@@ -1,78 +1,81 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CategoriesWidget extends StatelessWidget {
+  const CategoriesWidget({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Padding(
-        padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              "Category",
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-                color: Color(0xff00a368),
+    return Column(
+      children: [
+        const Padding(
+          padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Category",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff00a368),
+                ),
               ),
-            ),
-            Text(
-              "View All",
-              style: TextStyle(
-                fontSize: 15,
-                fontWeight: FontWeight.bold,
-                color: Colors.black54,
+              Text(
+                "View All",
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black54,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-      SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            for (int i = 0; i < 8; i++)
-              Container(
-                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                  height: 50,
-                  width: 50,
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              for (int i = 1; i <= 8; i++)
+                Container(
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  padding: const EdgeInsets.all(8),
                   decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 6,
-                          blurRadius: 7,
-                          // changes position of shadow
-                        ),
-                      ]),
-                  child: Row(children: [
-                    Padding(
-                      padding: EdgeInsets.all(5),
-                      child: Image.asset(
-                        "images/img4.jpg",
-                        height: 50,
-                        width: 50,
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        spreadRadius: 1,
+                        blurRadius: 5,
                       ),
-                    ),
-                    Padding(
-                        padding: EdgeInsets.only(right: 5),
-                        child: Text(
-                          "Fruits",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xff00a368),
-                          ),
-                        ))
-                  ]))
-          ],
+                    ],
+                  ),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Image.asset(
+                        "images/img${i}.png",
+                        height: 40,
+                        width: 40,
+                      ),
+                      const SizedBox(width: 8),
+                      const Text(
+                        "Category",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xff00a368),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+            ],
+          ),
         ),
-      )
-    ]);
+      ],
+    );
   }
 }
