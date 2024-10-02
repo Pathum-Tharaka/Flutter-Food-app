88d4815201f1883706e9c1fe997071b8ddba7867
pathumtharaka2000 <it22600516@my.sliit.lk>
2024-10-02 19:20:11 +0530

    Update ItemPage.dart

--- foodapp/lib/Pages/ItemPage.dart
+++ foodapp/lib/Pages/ItemPage.dart
@@ -1,8 +1,123 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ItemPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                  margin: EdgeInsets.all(15),
+                  height: 350,
+                  width: double.infinity,
+                  alignment: Alignment.topLeft,
+                  decoration: BoxDecoration(
+                    //color: Colors.red,
+                    image: DecorationImage(
+                      image: AssetImage("images/img2.png"),
+                    ),
+                  ),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Icon(
+                      Icons.arrow_back,
+                      size: 30,
+                      color: Color(0xff00a368),
+                    ),
+                  )),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.4,
+                margin: EdgeInsets.only(top: 20),
+                padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: Color(0xff00a368),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 10, bottom: 15),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Fruit Title",
+                            style: TextStyle(
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Container(
+                                padding: EdgeInsets.all(4),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(20),
+                                  // boxShadow: [
+                                  //   BoxShadow(
+                                  //     color: Colors.grey.withOpacity(0.5),
+                                  //     spreadRadius: 2,
+                                  //     blurRadius: 8,
+                                  //     offset: Offset(0, 3),
+                                  //   ),
+                                  // ]
+                                ),
+                                child: Icon(
+                                  CupertinoIcons.minus,
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.symmetric(horizontal: 10),
+                                child: Text(
+                                  "01",
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(3),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(20),
+                                  // boxShadow: [
+                                  //   BoxShadow(
+                                  //     color: Colors.grey.withOpacity(0.5),
+                                  //     spreadRadius: 2,
+                                  //     blurRadius: 8,
+                                  //     offset: Offset(0, 3),
+                                  //   ),
+                                  // ]
+                                ),
+                                child: Icon(
+                                  CupertinoIcons.plus,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
