eafb31ccf06decf4ea5d0ab48372164f6bb3c093
pathumtharaka2000 <it22600516@my.sliit.lk>
2024-10-03 14:55:32 +0530

    Update BottomBar.dart

--- foodapp/lib/widgets/BottomBar.dart
+++ foodapp/lib/widgets/BottomBar.dart
@@ -1,8 +1,54 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class BottomBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BottomAppBar(
+        child: Container(
+      height: 70,
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            "\$120",
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+              color: Color(0xff00a368),
+            ),
+          ),
+          InkWell(
+            onTap: () {},
+            child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                decoration: BoxDecoration(
+                  color: Color(0xff00a368),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.add_shopping_cart,
+                      color: Colors.white,
+                      size: 28,
+                    ),
+                    SizedBox(width: 5),
+                    Text(
+                      "Add to cart",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                )),
+          )
+        ],
+      ),
+    ));
   }
 }
